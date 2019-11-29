7aef5f3e7b9b1ba99d654b0a2de1d480255af47b
Matheus Moraes <matheusmoraes@MacBook-de-Matheus.local>
2019-11-29 10:47:00 -0300

    att

--- js/product.js
+++ js/product.js
@@ -1,8 +1,44 @@
-let value = $(".qtd-field").val();
+let sku = 0;
 
-let sku = dataLayer[0].productId;
+function BatchBuy_OnSkuDataReceived(e) {
+    var id = e.skuData.id;
+    selectedToBuy = [];
+    if (id > 0) {
+        console.log(e.skuData);
+        sku = e.skuData.id;
+    }
+}
+
+
+$(document).ready(function() {
+
+    if(skuJson.skus.length  == 1) {
+        console.log("KAWABUNGA")
+        sku = skuJson.skus[0].sku;
+    }
+
+    var batchBuyListener = new Vtex.JSEvents.Listener('batchBuyListener', BatchBuy_OnSkuDataReceived);
+    skuEventDispatcher.addListener(skuDataReceivedEventName, batchBuyListener);
+    $(".product-info .shipping-box .freight-btn").val("CALCULAR");
+
+
+    $(".go-to-cart-button").click(function(e){
+        
+        if (sku == 0) {
+            e.preventDefault();
+        alert("Selecione um SKU");
+        }
+    })
+
+
+
+
+
+    let value = $(".qtd-field").val();
+
+
 $(".go-to-cart-button").attr("href", `/checkout/cart/add?sku=${sku}&qty=1&seller=1&redirect=true&sc=1`);
-
+(".add-to-cart-button").attr("href", `/checkout/cart/add?sku=${sku}&qty=1&seller=1&redirect=true&sc=1`);
 
 $(".btn-mais").click(()=>{
     
@@ -19,6 +55,7 @@ $(".btn-menos").click(()=>{
 $(".qtd-field").on("input propertychange",()=>{
     let qty = $(".qtd-field").val();
     $(".go-to-cart-button").attr("href", `/checkout/cart/add?sku=${sku}&qty=${qty}&seller=1&redirect=true&sc=1`);
+    $(".add-to-cart-button").attr("href", `/checkout/cart/add?sku=${sku}&qty=${qty}&seller=1&redirect=false&sc=1`);
 
 
 })
@@ -26,11 +63,12 @@ $(".qtd-field").on("input propertychange",()=>{
 $(".btn-menos").click(()=>{
     let qty = $(".qtd-field").val();
     $(".go-to-cart-button").attr("href", `/checkout/cart/add?sku=${sku}&qty=${qty}&seller=1&redirect=true&sc=1`);
-
+    $(".add-to-cart-button").attr("href", `/checkout/cart/add?sku=${sku}&qty=${qty}&seller=1&redirect=false&sc=1`);
 })
 $(".btn-mais").click(()=>{
     let qty = $(".qtd-field").val();
     $(".go-to-cart-button").attr("href", `/checkout/cart/add?sku=${sku}&qty=${qty}&seller=1&redirect=true&sc=1`);
+    $(".add-to-cart-button").attr("href", `/checkout/cart/add?sku=${sku}&qty=${qty}&seller=1&redirect=false&sc=1`);
 
 })
 
@@ -65,8 +103,7 @@ $("#btn-reviews").click(()=>{
             });
     });
 })*/
-
-$(".add-to-cart-button").click(()=>{
+/*$(".add-to-cart-button").click(()=>{
     let item = {
         quantity: 1,
         seller: '1'
@@ -83,8 +120,27 @@ $(".add-to-cart-button").click(()=>{
             });
     });
      
-})
+})*/
 
+
+    
+});
+
+
+
+
+/*function BatchBuy_OnSkuDataReceived(e) {
+    var id = e.skuData.id;
+    selectedToBuy = [];
+    if (id > 0) {
+        console.log(e.skuData);
+    }
+}
+
+$(document).ready(function () {
+    var batchBuyListener = new Vtex.JSEvents.Listener('batchBuyListener', BatchBuy_OnSkuDataReceived);
+    skuEventDispatcher.addListener(skuDataReceivedEventName, batchBuyListener);
+});*/
 
 
 
